4103f6d3f6b7d0044af14452c9a704da69363443
LidiiaBogdanova <novikova71095@gmail.com>
2021-09-06 02:23:00 +0300

    webdrivermanager dependency

--- src/test/java/ru/netology/test/DebetCardRequestTest.java
+++ src/test/java/ru/netology/test/DebetCardRequestTest.java
@@ -1,5 +1,6 @@
 package ru.netology.test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,8 +20,9 @@ public class DebetCardRequestTest {
     private WebDriver driver;
 
     @BeforeAll
-    static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+    static void seUpAll() {
+        WebDriverManager.chromedriver().setup();
+       // System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
     }
 
     @BeforeEach
